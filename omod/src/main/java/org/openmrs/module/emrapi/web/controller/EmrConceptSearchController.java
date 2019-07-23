04f1e8c15a1ef6bda9b43d9f3f9ccaf23ec0ab08
IipperMangesh <mangeshi@satvix.com>
2019-07-23 17:03:50 +0530

    COM-328 import locale lib

--- omod/src/main/java/org/openmrs/module/emrapi/web/controller/EmrConceptSearchController.java
+++ omod/src/main/java/org/openmrs/module/emrapi/web/controller/EmrConceptSearchController.java
@@ -33,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.locale;
 
 @Controller
 @RequestMapping(method = RequestMethod.GET, value = "/rest/emrapi/concept")
